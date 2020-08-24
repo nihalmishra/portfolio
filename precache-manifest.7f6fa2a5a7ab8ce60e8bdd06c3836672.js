240dbb566b93988362325db1f1f73c3aefd84b2c
nihalmishra <nihal.mishra95@gmail.com>
2020-08-24 19:38:28 -0400

    Updates

---
+++ precache-manifest.7f6fa2a5a7ab8ce60e8bdd06c3836672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63667dda7a7e5f7095ae900ca1a83a2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f42d01117fb9550b8358",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42d01117fb9550b8358",
+    "url": "/portfolio/static/js/main.4b3c414f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
