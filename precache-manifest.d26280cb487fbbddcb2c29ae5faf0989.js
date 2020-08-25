13f0dd2e39af2c7c036f3baa3ac2c1ee0717a590
nihalmishra <nihalmishra@users.noreply.github.com>
2020-08-25 03:33:51 +0000

    deploy: 696e292b1577214d1f6d6d6e76e103b5815a2218

---
+++ precache-manifest.d26280cb487fbbddcb2c29ae5faf0989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f04d71f00e75d3bfc4f7e5ab9be25c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e370fb6bd6315b48d4dd",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e370fb6bd6315b48d4dd",
+    "url": "/portfolio/static/js/main.18d2e5f5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
